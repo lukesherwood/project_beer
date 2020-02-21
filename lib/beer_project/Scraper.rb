2b3fd90c3aa4a8f4c453fe789c010f900b3131a9
Luke Sherwood <luke.sherwood55@gmail.com>
2020-02-21 19:45:52 +0000

    added get cities scraper

--- lib/beer_project/Scraper.rb
+++ lib/beer_project/Scraper.rb
@@ -14,5 +14,16 @@ class BeerProject::Scraper
     BeerProject::Brewery.new(name, address, website)
     end
   end
+  
+  def self.scrape_cities
+    doc = Nokogiri::HTML(open("https://www.tripsavvy.com/search?q=best%20breweries"))
+
+    things = doc.css(".comp.l-container.search-results-list li")
+    things.each do |thing|
+      name = thing.css(".card__tag").attr("data-tag").text
+      website = thing.css(".comp.card").attr("href").text
+      BeerProject::Cities.new(name, website)
+    end
+  end
  
 end
