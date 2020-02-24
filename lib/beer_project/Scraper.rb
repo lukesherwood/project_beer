2559f28de0fbcdd2299b5877369aeed7ca5bdac9
Luke Sherwood <luke.sherwood55@gmail.com>
2020-02-24 18:10:33 +0000

    added other pages with cities

--- lib/beer_project/Scraper.rb
+++ lib/beer_project/Scraper.rb
@@ -13,7 +13,8 @@ class BeerProject::Scraper
   things.each do |thing|
       name =        thing.css(".mntl-sc-block-heading__text").text
       name =        thing.css(".mntl-sc-block-heading__link").text if name == "" #covers case if name is a link
-      website =     thing.css(".mntl-sc-block-location__website-text").attr("href")
+      website =     thing.css(".mntl-sc-block-location__website-text").attr("href") 
+      website =     thing.css(".mntl-sc-block-heading__link").attr("href") if website == "" #covers edge case when no website, but website in name link
       address =     thing.css(".mntl-sc-block-location__address").text.strip
       blurb =       thing.css(".comp.text-passage").text
       phone_number= thing.css(".mntl-sc-block-location__phone-text").text
@@ -22,13 +23,20 @@ class BeerProject::Scraper
   end
   
   def self.scrape_cities
-    doc = Nokogiri::HTML(open("https://www.tripsavvy.com/search?q=best%20breweries"))
+    @pages = ["https://www.tripsavvy.com/search?q=best%20breweries", "https://www.tripsavvy.com/search?q=best%20breweries&offset=24", "https://www.tripsavvy.com/search?q=best%20breweries&offset=48]"]
+    doc = Nokogiri::HTML(open(@pages[1]))
 
     things = doc.css(".comp.l-container.search-results-list li")
     things.each do |thing|
+      begin
       name = thing.css(".card__tag").attr("data-tag").text
       website = thing.css(".comp.card").attr("href").text
+      rescue NoMethodError
+        next
+      #need to add a error that misses cities without all information
+      else
       BeerProject::City.new(name, website)
+      end
     end
   end
  
