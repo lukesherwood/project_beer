6ee3a2adf487939efa0cd96bdd76f0b2a4f521c5
Luke Sherwood <luke.sherwood55@gmail.com>
2020-02-27 18:04:16 +0000

    trying to add open url

--- lib/beer_project.rb
+++ lib/beer_project.rb
@@ -8,6 +8,7 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 require 'rubygems'
+require 'launchy'
 
 module BeerProject
   class Error < StandardError; end
--- lib/beer_project/CLI.rb
+++ lib/beer_project/CLI.rb
@@ -1,17 +1,21 @@
 class BeerProject::CLI
   @input = ""
   def start
-    #need to start a loop
     while @input != 'exit' do
-    puts "Welcome to Find a Brewery CLI app"
-    puts "\nType exit at any time to close the app"
+    welcome
     cities
     get_user_city
     breweries
     get_user_brewery
-    gets_exit_or_restart
+    get_exit_or_restart
     end
   end
+  
+  def welcome
+    puts "Welcome to Find a Brewery CLI app"
+    puts "\nType exit at any time to close the app"
+  end
+    
   
   def cities
     puts "\nWhat city are you in?"
@@ -23,41 +27,8 @@ class BeerProject::CLI
     end
   end
   
-  def breweries
-    #second loop we get first loops breweries
-    puts "\nEnter the number of the brewery you want to know more about"
-    @breweries = BeerProject::Brewery.all
-    @breweries.each.with_index(1) do |brewery, index|
-      puts "\n#{index}. #{brewery.name}"
-      puts brewery.address unless brewery.address == ""
-    end
-  end
-  
-  def get_user_brewery
-    @input = gets.strip
-    if @input == "exit"
-      exit
-    else
-      begin
-      chosen_brewery_number = @input.to_i-1
-      rescue
-        puts "\nThat isn't a valid option"
-        sleep(1)
-        get_user_brewery
-      end
-      if valid_input?(chosen_brewery_number, @breweries)
-        show_brewery_info(chosen_brewery_number) 
-      else
-        puts "\nThat isn't a valid option"
-        sleep(1)
-        get_user_brewery
-      #can we combine these two error handlers into one?
-      end
-    end
-  end
-  #this is a duplicate from below can we abstract and combine?
   def get_user_city
-    @input = gets.strip
+    @input = gets.strip.downcase
     if @input == "exit"
       exit
     else
@@ -78,6 +49,39 @@ class BeerProject::CLI
       end
     end
   end
+  
+  def breweries
+    #second loop we get first loops breweries
+    puts "\nEnter the number of the brewery you want to know more about"
+    @breweries = BeerProject::Brewery.all
+    @breweries.each.with_index(1) do |brewery, index|
+      puts "\n#{index}. #{brewery.name}"
+      puts brewery.address unless brewery.address == ""
+    end
+  end
+  
+  def get_user_brewery
+    @input = gets.strip.downcase
+    if @input == "exit"
+      exit
+    else
+      begin
+      chosen_brewery_number = @input.to_i-1
+      rescue
+        puts "\nThat isn't a valid option"
+        sleep(1)
+        get_user_brewery
+      end
+      if valid_input?(chosen_brewery_number, @breweries)
+        show_brewery_info(chosen_brewery_number) 
+      else
+        puts "\nThat isn't a valid option"
+        sleep(1)
+        get_user_brewery
+      #can we combine these two error handlers into one?
+      end
+    end
+  end
   #this is a duplicate from above can we abstract and combine?
   
   def valid_input?(input, data)
@@ -94,11 +98,16 @@ class BeerProject::CLI
     #do we need an address shown here or on intial puts?
     puts @breweries[brewery_number].blurb
     puts "\nPhone:#{@breweries[brewery_number].phone_number}" unless @breweries[brewery_number].phone_number = ""
-    puts "\nFind more information at their website: #{@breweries[brewery_number].website}" unless @breweries[brewery_number].website == ""
+    #puts "\nFind more information at their website: #{@breweries[brewery_number].website}" unless @breweries[brewery_number].website == ""
+    puts "If you would like to go to their website type Y" unless @breweries[brewery_number].website == ""
+    @input = gets.strip.downcase
+    if @input == "y"
+      Launchy.open("#{@breweries[brewery_number].website}")
+    end
     #can we open website if they ask to?, or even open in maps with address?
   end
   
-  def gets_exit_or_restart
+  def get_exit_or_restart
     puts "\nDo you want to see any other cities? Y or exit"
     @input = gets.strip.downcase
     if @input == "y"
