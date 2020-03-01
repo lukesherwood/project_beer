96d7e9a19dd588839be26af2639e7e82eb41bbd7
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-01 22:46:03 +0000

    tidying up scraper

--- lib/beer_project/Scraper.rb
+++ lib/beer_project/Scraper.rb
@@ -25,45 +25,26 @@ class BeerProject::Scraper
   end
   
   def self.scrape_cities
-    self.scrape_cities_page1
-    self.scrape_cities_page2
-  end
-  
-  def self.scrape_cities_page1
     
     #need to condense this with page2
     @pages = ["https://www.tripsavvy.com/search?q=best%20breweries", "https://www.tripsavvy.com/search?q=best%20breweries&offset=24"]
     
-    doc = Nokogiri::HTML(open(@pages[0]))
-
-    things = doc.css(".comp.l-container.search-results-list li")
-    things.each do |thing|
-      name = thing.css(".card__tag").attr("data-tag").text
-      website = thing.css(".comp.card").attr("href").text
-      
-      not_cities = ["Things To Do", "Restaurants", "Nightlife", "Inspiration", "Neighborhoods", "Essentials", "Events", "Weird & Amazing", "Getaways", "Boston"]
-      BeerProject::City.new(name, website) unless not_cities.include? name
-      
-    end
-  end
-  
-  def self.scrape_cities_page2
-    @pages = ["https://www.tripsavvy.com/search?q=best%20breweries", "https://www.tripsavvy.com/search?q=best%20breweries&offset=24"]
+    doc = [Nokogiri::HTML(open(@pages[0])), Nokogiri::HTML(open(@pages[1]))]
     
-    doc = Nokogiri::HTML(open(@pages[1]))
-
-    things = doc.css(".comp.l-container.search-results-list li")
-    things.each do |thing|
-      begin
-      name = thing.css(".card__tag").attr("data-tag").text
-      website = thing.css(".comp.card").attr("href").text
-      rescue NoMethodError #this stops the error when cities don't have all the info
-        next
-      else
-      not_cities = ["Things To Do", "Restaurants", "Nightlife", "Inspiration", "Neighborhoods", "Essentials", "Events", "Weird & Amazing", "Getaways", "Boston"]
-      BeerProject::City.new(name, website) unless not_cities.include? name
+    cards = doc.each do |doc| 
+      cards = doc.css(".comp.l-container.search-results-list li")
+      cards.each do |attributes|
+        begin
+          name = attributes.css(".card__tag").attr("data-tag").text
+          website = attributes.css(".comp.card").attr("href").text
+          rescue NoMethodError #this stops the error when cities don't have all the info
+            next
+          else
+            not_cities = ["Things To Do", "Restaurants", "Nightlife", "Inspiration", "Neighborhoods", "Essentials", "Events", "Weird & Amazing", "Getaways", "Boston"]
+            BeerProject::City.new(name, website) unless not_cities.include? name
+        end
       end
     end
   end
- 
+  
 end
